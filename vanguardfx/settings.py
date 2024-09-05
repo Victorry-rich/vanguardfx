60ec15a6af19d95001325ac6f407f1abe14ace45
Victory Ricch <charlottealice113@gmail.com>
2024-09-05 13:41:36 +0100

    Update settings.py

--- vanguardfx/settings.py
+++ vanguardfx/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    'userauths.middleware.BypassCSRFForAdminMiddleware',
     # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
